bc547926bc37e6c708a3f2c2a1d934a8c11d94c1
thoradia <22841905+thoradia@users.noreply.github.com>
2019-12-20 10:11:40 +0100

    mono: update to 6.6.0.161

---
+++ packages/thoradia/addons/mono/package.mk
@@ -0,0 +1,59 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="mono"
+PKG_VERSION="6.6.0.161"
+PKG_SHA256="778badcf2f201df7efa5e031d01802ea8c29818de66e5e7e9f85212c2277d85f"
+PKG_REV="116"
+PKG_ARCH="any"
+PKG_LICENSE="MIT"
+PKG_SITE="http://www.mono-project.com"
+PKG_URL="http://download.mono-project.com/sources/mono/${PKG_NAME}-${PKG_VERSION}.tar.xz"
+PKG_DEPENDS_TARGET="toolchain mono:host sqlite zlib"
+PKG_TOOLCHAIN="autotools"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_NAME="Mono"
+PKG_ADDON_TYPE="xbmc.service"
+PKG_SECTION="tools"
+PKG_SHORTDESC="Mono: a cross platform, open source .NET framework"
+PKG_LONGDESC="Mono (${PKG_VERSION}) is a software platform designed to allow developers to easily create cross platform applications part of the .NET Foundation."
+PKG_MAINTAINER="thoradia"
+
+prefix=/storage/.kodi/addons/$PKG_SECTION.$PKG_NAME
+options="--build=${HOST_NAME} \
+         --prefix=${prefix} \
+         --bindir=${prefix}/bin \
+         --sbindir=${prefix}/sbin \
+         --sysconfdir=${prefix}/etc \
+         --libexecdir=${prefix}/lib \
+         --localstatedir=/var \
+         --disable-boehm \
+         --with-shared-mono=no \
+         --without-mcs-docs"
+
+configure_host() {
+  ../configure $options --host=${HOST_NAME}
+}
+
+makeinstall_host() {
+  :
+}
+
+configure_target() {
+  ../configure ${options} --host=${TARGET_NAME} \
+                          --disable-mcs-build
+}
+
+makeinstall_target() {
+  make -C ${PKG_BUILD}/.${HOST_NAME} install DESTDIR=${INSTALL}
+  make -C ${PKG_BUILD}/.${TARGET_NAME} install DESTDIR=${INSTALL}
+  $STRIP ${INSTALL}${prefix}/bin/* ${INSTALL}${prefix}/lib/* &> /dev/null || true
+}
+
+addon() {
+  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}
+  cd ${PKG_BUILD}/.install_pkg${prefix}
+    XZ_OPT=-9e tar -cJf ${ADDON_BUILD}/${PKG_ADDON_ID}/${PKG_SECTION}.${PKG_NAME}.tar.xz *
+  cd -
+}
