2e2ea5bdd9d9bc22f2db0da76c6645f09486b91a
thoradia <22841905+thoradia@users.noreply.github.com>
2019-12-20 09:21:52 +0100

    pcre2: version 10.34

---
+++ packages/thoradia/depends/pcre2/package.mk
@@ -0,0 +1,13 @@
+PKG_NAME="pcre2"
+PKG_VERSION="10.34"
+PKG_SHA256="74c473ffaba9e13db6951fd146e0143fe9887852ce73406a03277af1d9b798ca"
+PKG_LICENSE="BSD"
+PKG_SITE="http://www.pcre.org/"
+PKG_URL="https://ftp.pcre.org/pub/pcre/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="The PCRE library is a set of functions that implement regular expression pattern matching using the same syntax and semantics as Perl 5. PCRE has its own native API, as well as a set of wrapper functions that correspond to the POSIX regular expression API. The PCRE library is free, even for building commercial software."
+
+PKG_BUILD_FLAGS="+pic"
+PKG_CMAKE_OPTS_TARGET="-DPCRE2_BUILD_PCRE2_16=ON \
+                       -DPCRE2_BUILD_PCRE2GREP=OFF \
+                       -DPCRE2_BUILD_TESTS=OFF"
