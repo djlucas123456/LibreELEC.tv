1ef4f6ff7de75cfd789c434b81579cfd85627abc
thoradia <22841905+thoradia@users.noreply.github.com>
2019-12-20 09:20:00 +0100

    libevent: version 2.1.11-stable

---
+++ packages/thoradia/depends/libevent/package.mk
@@ -0,0 +1,15 @@
+PKG_NAME="libevent"
+PKG_VERSION="2.1.11-stable"
+PKG_SHA256="229393ab2bf0dc94694f21836846b424f3532585bac3468738b7bf752c03901e"
+PKG_LICENSE="BSD"
+PKG_SITE="http://libevent.org/"
+PKG_URL="https://github.com/libevent/${PKG_NAME}/archive/release-${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain openssl zlib"
+PKG_LONGDESC="libevent: a library for asynchronous event notification"
+
+PKG_CMAKE_OPTS_TARGET="-DEVENT__DISABLE_BENCHMARK=ON \
+                       -DEVENT__DISABLE_DEBUG_MODE=ON \
+                       -DEVENT__DISABLE_REGRESS=ON \
+                       -DEVENT__DISABLE_SAMPLES=ON \
+                       -DEVENT__DISABLE_TESTS=ON \
+                       -DEVENT__LIBRARY_TYPE=STATIC"
