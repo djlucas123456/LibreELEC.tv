6cd45baceee837804ceaf8d68e080c76fe2d7b8f
thoradia <22841905+thoradia@users.noreply.github.com>
2019-12-20 02:05:22 +0100

    headphones: broken, requires python2

---
+++ packages/thoradia/addons/headphones/package.mk
@@ -0,0 +1,22 @@
+PKG_NAME="headphones"
+PKG_VERSION="0.0"
+PKG_REV="0"
+PKG_LICENSE=""
+PKG_SITE=""
+PKG_URL=""
+PKG_DEPENDS_TARGET="toolchain"
+PKG_TOOLCHAIN="manual"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_NAME="Headphones"
+PKG_ADDON_TYPE="xbmc.broken"
+PKG_ADDON_BROKEN="${PKG_ADDON_NAME} has been removed"
+PKG_SECTION="service"
+PKG_SHORTDESC="${PKG_ADDON_NAME} has been removed"
+PKG_LONGDESC="${PKG_ADDON_NAME} requires Python 2, which is out of support and no longer provided."
+PKG_MAINTAINER="thoradia"
+PKG_DISCLAIMER="Keep it legal and carry on"
+
+addon() {
+  :
+}
