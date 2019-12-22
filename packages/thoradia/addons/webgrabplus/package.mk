6f52dda7079563ed0b63af537491926f774de281
thoradia <22841905+thoradia@users.noreply.github.com>
2019-12-22 17:30:29 +0100

    webgrabplus: rework

---
+++ packages/thoradia/addons/webgrabplus/package.mk
@@ -0,0 +1,28 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="webgrabplus"
+PKG_VERSION="2.1.11_beta"
+PKG_SHA256="9065c28e8556da4f33257aa9f4162677642cd026a80980769ab13b240c605f86"
+PKG_REV="111"
+PKG_ARCH="any"
+PKG_LICENSE="prop."
+PKG_SITE="http://www.webgrabplus.com/"
+PKG_URL="http://webgrabplus.com/sites/default/files/download/SW/V2.1.11/WebGrabPlus_V2.1.11_beta_install.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_TOOLCHAIN="manual"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_NAME="WebGrab+Plus"
+PKG_ADDON_TYPE="xbmc.service"
+PKG_SECTION="service"
+PKG_SHORTDESC="WebGrab+Plus: a multi-site incremental xmltv epg grabber"
+PKG_LONGDESC="WebGrab+Plus (${PKG_VERSION}) collects tv-program guide data from selected tvguide sites for your favourite channels."
+PKG_ADDON_REQUIRES="tools.mono:0.0.0"
+PKG_MAINTAINER="thoradia"
+
+addon() {
+  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}
+    cp -r ${PKG_BUILD}/bin \
+          ${ADDON_BUILD}/${PKG_ADDON_ID}
+}
